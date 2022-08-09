afc681279413138769371fec592110b0836ef65b
SeniorNet <109102047+SeniorNet@users.noreply.github.com>
2022-08-09 03:02:55 -0400

    initial version

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Microsoft_365_PoC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
